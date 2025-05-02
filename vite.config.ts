88c258def507ba22f2115283285cb90c235386f2
TrayMachi <tristan.agra@ui.ac.id>
2025-05-02 15:22:02 +0700

    build: fly  config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT || "3000"),
+  },
 });
